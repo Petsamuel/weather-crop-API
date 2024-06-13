849f69fe69eeb26cb4396c33cf4cc4b5d92e8ddf
petsamuel <petsamuel4@gmail.com>
2024-06-13 21:09:03 +0100

    chore: Update weather API URL variable name

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from models import WeatherData, Coordinates
 from typing import List, Dict
+
 from retry_requests import retry
 import pandas as pd
 
@@ -22,7 +23,7 @@ WEATHER_API_URL = os.getenv("WEATHER_API_URL")
 GEOCODING_API_URL = os.getenv("GEOCODING_API_URL")
 FORECAST_API_URL = os.getenv("FORECAST_API_URL")
 WEATHER_API_KEY2 = os.getenv("WEATHER_API_KEY2")
-WEATHER_HISTORICAL_API_URL = os.getenv("WEATHE_HISTORICAL_API_URL")
+WEATHER_HISTORICAL_API_URL = os.getenv("WEATHER_HISTORICAL_API_URL")
 # Load crop data from crops.json
 with open("crops.json", "r") as f:
     crop_data = json.load(f)
@@ -33,24 +34,27 @@ cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
-# Root endpoint
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Weather Crop API"}
 
+# helper function 
 
-# Get weather and recommendations for a city
-@app.get("/weather/{city}", status_code=200)
-def get_weather_and_crop_recommendations(city: str):
-    try:
-        lat, lon = get_coordinates(city)
-        weather_data = get_weather(lat, lon)
-        recommended_crops = recommend_crops(weather_data.dict(), crop_data)
-        return {"status": "success", "data": weather_data.dict(), "recommended_crops": recommended_crops}
-    except HTTPException as e:
-        return {"status": "error", "detail": e.detail}
+def get_weather(lat: float, lon: float):
+    params = {
+        'lat': lat,
+        'lon': lon,
+        'appid': WEATHER_API_KEY,
+        'units': 'metric'
+    }
+    response = requests.get(WEATHER_API_URL, params=params)
+    if response.status_code != 200:
+        raise HTTPException(status_code=404, detail="Weather data not found")
+    data = response.json()
+    return WeatherData(
+        temp=data['main']['temp'],
+        humidity=data['main']['humidity'],
+        wind_speed=data['wind']['speed'],
+        description=data['weather'][0]['description']
+    )
 
-# Get coordinates of a city
 def get_coordinates(city: str):
     params = {
         'q': f"{city},NG",
@@ -62,7 +66,6 @@ def get_coordinates(city: str):
         raise HTTPException(status_code=404, detail="City not found or not in Nigeria")
     data = response.json()[0]
     return data['lat'], data['lon']
-
 # Get weather data for given coordinates
 def get_weather(lat: float, lon: float):
     params = {
@@ -103,7 +106,45 @@ def recommend_crops(weather_data, crop_data):
     else:
         print("Default")
         return crop_data.get("default", [])
+    
+    
+def current_weather(lat: float, lon: float):
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "current": ["temperature_2m", "apparent_temperature", "is_day", "precipitation", "rain", "cloud_cover", "surface_pressure", "wind_speed_10m", "wind_direction_10m", "wind_gusts_10m"],
+	    "hourly": "temperature_2m"
+    }
+    responses = requests.get(WEATHER_API_KEY2, params=params)
+    if responses.status_code == 200:
+        data = responses.json()
+        print("Full API response:", data['current'])
+        # Extract specific variables from the current weather data
+        current_data = data['current']
+        return current_data
+    else:
+        # Handle the case where the API response is not successful
+        return {"error": "Failed to retrieve weather data"}
+    
+    
+# Root endpoint
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Weather Crop API"}
 
+
+# Get weather and recommendations for a city
+@app.get("/weather/{city}", status_code=200)
+def get_weather_and_crop_recommendations(city: str):
+    try:
+        lat, lon = get_coordinates(city)
+        weather_data = get_weather(lat, lon)
+        recommended_crops = recommend_crops(weather_data.dict(), crop_data)
+        return {"status": "success", "data": weather_data.dict(), "recommended_crops": recommended_crops}
+    except HTTPException as e:
+        return {"status": "error", "detail": e.detail}
+
+    
 # api for forecasting
 @app.get("/weather/forecast/{city}", status_code=200)
 def get_weather_forecast_and_crop_recommendations(city: str):
@@ -133,82 +174,15 @@ def get_weather_forecast(lat: float, lon: float):
         description=data['weather'][0]['description']
     )
 
-def get_coordinates(city: str):
-    params = {
-        'q': f"{city},NG",
-        'limit': 1,
-        'appid': WEATHER_API_KEY
-    }
-    response = requests.get(GEOCODING_API_URL, params=params)
-    if response.status_code != 200 or len(response.json()) == 0:
-        raise HTTPException(status_code=404, detail="City not found or not in Nigeria")
-    data = response.json()[0]
-    return data['lat'], data['lon']
 
-def recommend_crops(weather_data, crop_data):
-    temp = weather_data["temp"]
-    humidity = weather_data["humidity"]
-    description = weather_data["description"].lower()
-    recommended = []
+@app.get("/weather/current/{city}", status_code=200)
+def get_weather_current(city: str):
+    lat, lon = get_coordinates(city)
+    current_data = current_weather(lat, lon)
 
-    if temp > 30 and humidity < 50:
-        recommended = crop_data["high_temp_low_humidity"]
-    elif 20 <= temp <= 30 and humidity > 60:
-        recommended = crop_data["moderate_temp_high_humidity"]
-    elif temp < 20:
-        recommended = crop_data["low_temp"]
-    elif "rain" in description:
-        recommended = crop_data["rainy"]
-    else:
-        recommended = crop_data["default"]
-    return recommended
+    
+    return {"status": "success", "data": current_data}
 
-# api for getting weather data for a single city
-@app.get("/weather/history/{city}", status_code=200)
-def get_weather_history(city: str):
-    try:
-        lat, lon = get_coordinates(city)
-        weather_data = get_weather_histories(lat, lon)
-        return {"status": "success", "data": weather_data}
-    except HTTPException as e:
-        return {"status": "error", "detail": e.detail}
-
-def get_coordinates(city: str):
-    params = {
-        'q': f"{city},NG",
-        'limit': 1,
-        'appid': WEATHER_API_KEY
-    }
-    response = requests.get(GEOCODING_API_URL, params=params)
-    if response.status_code != 200 or len(response.json()) == 0:
-        raise HTTPException(status_code=404, detail="City not found or not in Nigeria")
-    data = response.json()[0]
-    return data['lat'], data['lon']
-
-def get_weather_histories(lat: float, lon: float):
-    params = {
-        "latitude": lat,
-        "longitude": lon,
-        "start_date": "2024-05-26",
-        "end_date": "2024-06-09",
-        "hourly": "temperature_2m"
-    }
-    responses = openmeteo.weather_api(WEATHER_HISTORICAL_API_URL, params=params)
-    response = responses[0]
-    hourly = response.Hourly()
-    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-
-    hourly_data = {"date": pd.date_range(
-        start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
-        end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
-        freq = pd.Timedelta(seconds = hourly.Interval()),
-        inclusive = "left"
-    )}
-    hourly_data["temperature_2m"] = hourly_temperature_2m
-
-    hourly_dataframe = pd.DataFrame(data = hourly_data)
-    daily_data = hourly_dataframe.resample("D", on="date").mean()
-    return daily_data.to_dict(orient='records')
     
 
     
