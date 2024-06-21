e23874b86e9cf1f78ba991cd1a9910dcee3939a1
petsamuel <petsamuel4@gmail.com>
2024-06-21 14:52:43 +0100

    chore: Add CORS middleware for specified origins

--- main.py
+++ main.py
@@ -10,11 +10,23 @@ from models import WeatherData, Coordinates
 from typing import List, Dict
 from retry_requests import retry
 import pandas as pd
-
+from fastapi.middleware.cors import CORSMiddleware
 
 # Load environment variables from the .env file
 app = FastAPI()
 load_dotenv()
+origins = [
+    "http://localhost:5173",  # Add your frontend's URL here
+    "https://weather-crop-api.vercel.app"  
+]
+#middleware app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 
